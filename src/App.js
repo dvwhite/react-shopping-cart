e86352aa0679159449157514781ef5d91568c8f4
David White <dwhite0627@gmail.com>
2020-03-05 08:40:42 -0500

    Move addItem to the Product component, tidy up whitespace

--- src/App.js
+++ src/App.js
@@ -6,26 +6,21 @@ import data from "./data";
 import Navigation from "./components/Navigation";
 import Products from "./components/Products";
 import ShoppingCart from "./components/ShoppingCart";
+
+// Contexts
 import { ProductContext } from "./contexts/ProductContext";
 import { CartContext } from "./contexts/CartContext";
 
 function App() {
   const [products] = useState(data);
   const [cart, setCart] = useState([]);
-
-  const addItem = item => {
-    setCart([...cart, item]);
-  };
-
   return (
-    <ProductContext.Provider value={{ products, addItem }}>
-      <CartContext.Provider value={{cart}}>
+    <ProductContext.Provider value={{ products }}>
+      <CartContext.Provider value={{cart, setCart}}>
         <div className="App">
           <Navigation />
-
           {/* Routes */}
           <Route exact path="/" component={Products} />
-
           <Route path="/cart" component={ShoppingCart} />
         </div>
       </CartContext.Provider>
--- src/components/ShoppingCartItem.js
+++ src/components/ShoppingCartItem.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { CartContext } from '../contexts/CartContext';
 
 const Item = props => {
+  
+  // Remove from cart function
+  const { cart, setCart } = useContext(CartContext);
+
+  const removeItem = (id) => {
+    console.log(cart.findIndex(item => item.id === id))
+    console.log(cart.splice(cart.findIndex(item => item.id === id)))
+    // setCart([..., 1])
+  }
+
 	return (
 		<div className="shopping-cart_item">
 			<img src={props.image} alt={`${props.title} book`} />
-
-
 			<div>
 				<h1>{props.title}</h1>
 				<p>$ {props.price}</p>
-				<button>Remove from cart</button>
+				<button onClick={() => removeItem(props.id)}>Remove from cart</button>
 			</div>
 		</div>
 	);
